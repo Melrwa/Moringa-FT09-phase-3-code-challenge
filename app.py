1b1f36042a22313e78019963640da29f250b2a19
melrwa <orwamelkizedek@gmail.com>
2024-12-16 01:29:10 +0300

    refactored the code to sqlAlchemy

--- app.py
+++ app.py
@@ -1,70 +1,48 @@
-from database.setup import create_tables
-from database.connection import get_db_connection
-from models.article import Article
+# app.py
+from database.setup import engine, Session, create_tables
 from models.author import Author
 from models.magazine import Magazine
+from models.article import Article
 
-def main():
-    # Initialize the database and create tables
-    create_tables()
+# Print all mapped tables
+from database.setup import Base
+# print(Base.metadata.tables)
 
-    # Collect user input
-    author_name = input("Enter author's name: ")
-    magazine_name = input("Enter magazine name: ")
-    magazine_category = input("Enter magazine category: ")
-    article_title = input("Enter article title: ")
-    article_content = input("Enter article content: ")
+# Initialize the database and create tables
+create_tables()
 
-    # Connect to the database
-    conn = get_db_connection()
-    cursor = conn.cursor()
+# Create a session
+session = Session()
+
+# Create some authors and magazines
+author = Author(name="Jane Doe")
+magazine = Magazine(name="Tech Monthly", category="Technology")
+
+# Add authors and magazines to the session and commit
+session.add(author)
+session.add(magazine)
+session.commit()
+
+# Create articles
+article1 = Article(author=author, magazine=magazine, title="The Future of AI")
+article2 = Article(author=author, magazine=magazine, title="Quantum Computing: A Primer")
+session.add(article1)
+session.add(article2)
+session.commit()
+
+# Fetch the first magazine and its associated data
+magazine = session.query(Magazine).first()
+
+# Fetch authors who contributed to the magazine using the relationship
+authors = magazine.get_contributors()  # Correct method name here
+
+# Fetch article titles from the magazine using the relationship
+titles = magazine.get_article_titles()
+
+# Print out authors and article titles
+print("Authors:", authors)
+print("Article Titles:", titles)
 
 
-    '''
-        The following is just for testing purposes, 
-        you can modify it to meet the requirements of your implmentation.
-    '''
-
-    # Create an author
-    cursor.execute('INSERT INTO authors (name) VALUES (?)', (author_name,))
-    author_id = cursor.lastrowid # Use this to fetch the id of the newly created author
-
-    # Create a magazine
-    cursor.execute('INSERT INTO magazines (name, category) VALUES (?,?)', (magazine_name, magazine_category))
-    magazine_id = cursor.lastrowid # Use this to fetch the id of the newly created magazine
-
-    # Create an article
-    cursor.execute('INSERT INTO articles (title, content, author_id, magazine_id) VALUES (?, ?, ?, ?)',
-                   (article_title, article_content, author_id, magazine_id))
-
-    conn.commit()
-
-    # Query the database for inserted records. 
-    # The following fetch functionality should probably be in their respective models
-
-    cursor.execute('SELECT * FROM magazines')
-    magazines = cursor.fetchall()
-
-    cursor.execute('SELECT * FROM authors')
-    authors = cursor.fetchall()
-
-    cursor.execute('SELECT * FROM articles')
-    articles = cursor.fetchall()
-
-    conn.close()
-
-    # Display results
-    print("\nMagazines:")
-    for magazine in magazines:
-        print(Magazine(magazine["id"], magazine["name"], magazine["category"]))
-
-    print("\nAuthors:")
-    for author in authors:
-        print(Author(author["id"], author["name"]))
-
-    print("\nArticles:")
-    for article in articles:
-        print(Article(article["id"], article["title"], article["content"], article["author_id"], article["magazine_id"]))
-
-if __name__ == "__main__":
-    main()
+# Close the session
+session.close()
--- database/connection.py
+++ database/connection.py
@@ -1,8 +1,18 @@
-import sqlite3
+# import sqlite3
 
-DATABASE_NAME = './database/magazine.db'
+# DATABASE_NAME = './database/magazine.db'
 
-def get_db_connection():
-    conn = sqlite3.connect(DATABASE_NAME)
-    conn.row_factory = sqlite3.Row
-    return conn
+# def get_db_connection():
+#     conn = sqlite3.connect(DATABASE_NAME)
+#     conn.row_factory = sqlite3.Row
+#     return conn
+
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+DATABASE_URL = "sqlite:///magazines.db"  # Use your actual DB connection URL
+
+engine = create_engine(DATABASE_URL)
+Session = sessionmaker(bind=engine)
+
